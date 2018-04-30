46572c096a5c31c8f1b0f9d7677f6a959b16c1b6
Ralf S. Engelschall <rse@engelschall.com>
2018-04-30 20:50:45 +0200

    improve MPM test

--- sample/mpm.js
+++ sample/mpm.js
@@ -6,14 +6,14 @@ const cluster = require("cluster")
 
 ;(async () => {
     if (cluster.isMaster) {
-        for (let i = 0; i < 1; i++)
+        for (let i = 0; i < 2; i++)
             cluster.fork()
         cluster.on("exit", (worker, code, signal) => {
             console.log(`DIED ${worker.process.pid}`)
         })
     }
     setTimeout(async () => {
-        await suite("mpm:foo")
+        await suite(`mpm:foo:${process.pid}`)
         if (!cluster.isMaster)
             process.exit(0)
     }, cluster.isMaster ? 1000 : 3000)
