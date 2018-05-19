c1fc4e6ff061f679440f92662c20b166dfc73508
Ralf S. Engelschall <rse@engelschall.com>
2018-05-19 18:30:39 +0200

    cleanup sample

--- sample/rpm.js
+++ sample/rpm.js
@@ -2,10 +2,10 @@
 const suite = require("./suite")
 
 ;(async () => {
-    // await suite("rpm+sqlite:///test.db?table=kv&key=colKey&colVal=val")
-    // await suite("rpm+pgsql://postgresql:postgresql@127.0.0.1:5432/example?table=kv&colKey=key&colVal=val")
+    await suite("rpm+sqlite:///test.db?table=kv&key=colKey&colVal=val")
+    await suite("rpm+pgsql://postgresql:postgresql@127.0.0.1:5432/example?table=kv&colKey=key&colVal=val")
     await suite("rpm+mysql://example:example@127.0.0.1:3306/example?table=kv&colKey=name&colVal=val")
-    // await suite("rpm+redis://x:local-secret@127.0.0.1:6379/test")
+    await suite("rpm+redis://x:local-secret@127.0.0.1:6379/test")
 })().catch((err) => {
     console.log(`ERROR: ${err}`)
 })
